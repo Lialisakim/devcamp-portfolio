f3fbce93a5d707c81267c4adc3c3fac205701492
May Phyo <mayeieiphyo69@gmail.com>
2021-12-23 05:56:45 +0630

    Added new parameters for user registration and editing account

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+# Pick the frameworks you want:
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
