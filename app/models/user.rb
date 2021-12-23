8774eeb1f7669f2314ed98534bfe0c6253d03e67
May Phyo <mayeieiphyo69@gmail.com>
2021-12-23 14:15:14 +0630

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    #User.last => John Smith
+    self.name.split.first
+  end
+
+  def last_name
+     self.name.split.last
+  end
+
 end
+
