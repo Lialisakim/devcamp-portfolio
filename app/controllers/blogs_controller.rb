6dd52cb6aa4ce411c243bd6005f7bb541de2a56d
May Phyo <mayeieiphyo69@gmail.com>
2021-12-28 05:29:49 +0630

    Added nav bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
+    @page_title="My Portfolio"
   end
 
   # GET /blogs/1 or /blogs/1.json
--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,6 @@ module GeneratorApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-  end
+    config.action_controller.permit_all_parameters = true
+    end
 end
